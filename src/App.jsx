8dd44213a41a49e88be6e4d468bdb2c1bce0d187
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-08-09 14:28:03 +0000

    added dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import Die from './components/Die.jsx'
+import { useState } from 'react'
+
 export default function App() {
+
+    const [dice, setDice] = useState(generateAllNewDice());
+    
+    function generateAllNewDice() {
+        return new Array(10).fill(0).map((_, index) => {
+            return <Die id={index} value={Math.ceil(Math.random()*6)}/>
+        });
+    }
+
+    generateAllNewDice();
     return (
         <main>
-            <h1>Tenzies</h1>
+            <div className='die-container'>
+                {dice}
+            </div>
         </main>
     );
 }
